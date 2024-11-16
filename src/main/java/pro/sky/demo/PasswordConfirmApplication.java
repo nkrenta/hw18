983c4391ffdb31fa73340a06f51baf71e81a8b7c
origin <nkrenta@inbox.ru>
2024-11-16 21:14:07 +0300

    Init

---
+++ src/main/java/pro/sky/demo/PasswordConfirmApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordConfirmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordConfirmApplication.class, args);
+	}
+
+}
